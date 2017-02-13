8c1b5ea5229e52c551f95d082e7e85e4b7c4770d
MuchaB <kierumucha@outlook.com>
2017-02-13 22:43:11 +0300

    Step 3 No error but no sign of stuff being saved

---
+++ app/src/main/java/com/ben/muchab/helloworld/TestClass.java
@@ -0,0 +1,11 @@
+package com.ben.muchab.helloworld;
+
+/**
+ * Created by MuchaB on 2/13/2017.
+ */
+
+public class TestClass {
+
+    //Folders not showing in github, Just a bunch of code Test 1
+
+}
